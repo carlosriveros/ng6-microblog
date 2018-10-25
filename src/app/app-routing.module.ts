637969d56cf010dfc1e0f13e7c4a92992deab1de
carlos riveros <carlos.riveros@paytm.com>
2018-10-25 14:28:53 -0400

    adding components and styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import UsersComponent from './users/users.component'
+import DetailsComponent from './details/details.component'
+import PostsComponent from './posts/posts.component'
+
 
 const routes: Routes = [];
 
